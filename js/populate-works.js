2a82e8787995cae669ed9b2ccb50ff4885079751
Joshua Ho Gwok Hin <hgh.joshua@gmail.com>
2024-02-25 11:13:15 +0800

    Update list-items placement and styling

--- js/populate-works.js
+++ js/populate-works.js
@@ -15,48 +15,49 @@ const populateworks = (inputArray) => {
         let links = inputArray[i]["links"];
 
         stringHTML += `
-            <article class="row mt-5">
+            <article class="row mt-3 mb-4">
                 <h2 class="py-3 text-info console-font">${title}</h2>
                 <section class="col-12 col-md-6 col-lg-4 border-right">
-                    <img src="${src}" alt="" class="w-100">
+                    <img src="${src}" alt="" class="w-100 mb-3">
                 </section>
                 <section class="col-12 col-md-6 col-lg-6 col-xl-4">
                     <h3 class="">Descriptions:</h3>
                     <p>${descriptions}</p>
                     <h4 class="">Languages:</h4>
-                    <ul>`;
+                    <ul class="list-unstyled">`;
 
         for (let j = 0; j < languages.length; j++) {
             stringHTML += `
-                        <li>${languages[j]}</li>`;
+                        <li class="d-inline pe-2">${languages[j]}</li>`;
         };
 
         stringHTML += `
                     </ul>
                     <h4 class="">Skills:</h4>
-                    <ul>`;
+                    <ul class="list-unstyled">`;
 
         for (let k = 0; k < skills.length; k++) {
             stringHTML += `
-                        <li>${skills[k]}</li>`;
+                        <li class="d-inline pe-2">${skills[k]}</li>`;
         };
 
         stringHTML += `
                     </ul>
-                    <h4 class="">Demo Links:</h4>`;
+                    <h4 class="mb-3">Demo Links:</h4>`;
 
         for (let l = 0; l < links.length; l++) {
             let linksObject = links[l];
             for (let key in linksObject){
            stringHTML += `
                     <a href="${linksObject[key]}"
-                    target="_blank" class="btn btn-dark">${key}</a>`;
+                    target="_blank" class="btn btn-dark me-1"><i class="bi bi-box-arrow-up-right"></i>&ThickSpace;${key}</a>`;
             }
         };
 
         stringHTML += `
                 </section>
-            </article>`;
+            </article>
+            <hr class="border-1 border-top border-white">`;
     };
 
     displayArray.innerHTML = stringHTML;
